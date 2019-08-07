aea595eda4fee4b2cfdc22a9d2d6a0c0908bbfc0
BIT <BIT@DESKTOP-7P1ENO8>
2019-08-08 08:55:11 +0900

    2번 수정 완료

--- src/prob02/Prob02.java
+++ src/prob02/Prob02.java
@@ -7,21 +7,16 @@ public class Prob02 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println( "5개의 숫자를 입력하세요." );
-		
+
 		int intArray[] = new int[5];
-			
+
 		double sum = 0;
-		
+
 		for (int i = 0; i < intArray.length; i++) {
 			intArray[i]=scanner.nextInt();
 			sum += intArray[i];
-		}		
-		
-		for(int i=0; i < intArray.length; i++) {
-			sum += intArray[i]; 
-			System.out.println(intArray[i]);
 		}
-		
+
 		double average = sum / intArray.length;
 		System.out.println("평균은" + average + "입니다.");
 		scanner.close();
